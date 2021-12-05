7b4f73250500b3c4ed548905ad634c3f7524d801
AlexBelikau <belikovgml@gmail.com>
2021-12-05 23:35:04 +0300

    new file

---
+++ 8title.js
@@ -0,0 +1,9 @@
+﻿// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
